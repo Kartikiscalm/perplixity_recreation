b9cdd06698aa4f23db0b29d252e0f65bbabe29bc
Kartikiscalm <kartikjayantai@gmail.com>
2026-07-09 22:49:13 +0530

    dashboard

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../wigets/side_bar.dart';
 import '../wigets/search_section.dart';
+import 'package:perplixity_recreation/theme/colours.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -11,11 +12,41 @@ class Homepage extends StatelessWidget {
       body: Row(
         children: [
           SideBar(),
-          Column(
-            children: [
-              SearchSection(), // search section
-              // footer
-            ],
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(child: SearchSection()), // search section
+                // footer
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "llm used is gemini 2.5 flash",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "github username: Kartikiscalm",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
