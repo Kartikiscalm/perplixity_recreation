4a948e854697f56c323d8b6b95c826a86dff8e38
Kartikiscalm <kartikjayantai@gmail.com>
2026-07-19 16:12:39 +0530

    backend sorted

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import FastAPI, WebSocket
 import uvicorn
 
@@ -19,15 +21,19 @@ async def websocket_chat_endpoint(websocket: WebSocket):
     await websocket.accept()
 
     try:
+        await asyncio.sleep(0.1)
         data = await websocket.receive_json()
         print(data)
         query = data.get("query")
+        print(query)
 
         #serch the web and return the sources
         search_results = search_service.web_search(query)
+        print(search_results)
         #sort the sources and get the most relivent ones
         sorted_results = sort_source_service.sort_sources(query, search_results)
         print(sorted_results)
+        await asyncio.sleep(0.1)
         await websocket.send_json({
             "type":"search_result",
             "data": sorted_results
@@ -36,6 +42,7 @@ async def websocket_chat_endpoint(websocket: WebSocket):
         
         #gen the response using llm
         for chunk in llm_service.generate_response(query,sorted_results):
+            await asyncio.sleep(0.1)
             await websocket.send_json({
                 "type": "content",
                 "data":chunk
--- backend/services/sort_source_service.py
+++ backend/services/sort_source_service.py
@@ -8,17 +8,20 @@ class SortSourceService:
         self.embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
     def sort_sources(self, query:str, search_results: List[dict]):
-        relevent_docs = []
-        query_embedding = self.embedding_model.encode(query)
+        try:
+            relevent_docs = []
+            query_embedding = self.embedding_model.encode(query)
 
-        for res in search_results:
-            res_embeddings = self.embedding_model.encode(res['content'] or "")
-            # np.linalg.norm cal the magnitude of the query_emd and the res_emd
-            similarity = np.dot(query_embedding, res_embeddings)/(np.linalg.norm(query_embedding) * np.linalg.norm(res_embeddings))
+            for res in search_results:
+                res_embeddings = self.embedding_model.encode(res['content'] or "")
+                # np.linalg.norm cal the magnitude of the query_emd and the res_emd
+                similarity = float(np.dot(query_embedding, res_embeddings)/(np.linalg.norm(query_embedding) * np.linalg.norm(res_embeddings)))
 
-            res['relevence_score'] = similarity
+                res['relevence_score'] = similarity
 
-            if similarity > 0.3:
-                relevent_docs.append(res)
+                if similarity > 0.3:
+                    relevent_docs.append(res)
 
-        return sorted(relevent_docs, key= lambda x: x['relevence_score'], reverse = True)
+            return sorted(relevent_docs, key= lambda x: x['relevence_score'], reverse = True)
+        except Exception as e:
+            print(e)
